bb1bd32cff936a8d9dd54818d5a83865366559f1
Edson <britoedso@gmail.com>
2021-12-02 10:25:49 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,67 @@
+// Imports express
+
+const e = require('express')
+const express = require('express')
+const app = express()
+const port = 3000
+
+// sets data.json to projects
+
+const projects = require('./data.json')
+
+/**
+ *  Sets the static route
+ *  Sets the view engine to pug
+ */
+
+app.use('/static', express.static('public'))
+app.set('view engine', 'pug')
+
+// Root page route. Lists all of the projects
+app.get('/', (req, res) => {
+  res.render('index', projects)
+})
+
+// Sets about route.
+app.get('/about', (req, res) => {
+  res.render('about')
+})
+
+// project route. Displays each project in the data.json file.
+app.get('/project/:id', (req, res, next) => {
+  const project = projects.projects[global.parseInt(req.params.id - 1)]
+  if (project !== undefined) {
+    res.render('project', { project })
+  } else {
+    const err = new Error()
+    err.status = 404
+    err.message = 'The page you are looking for does not exist'
+    next(err)
+  }
+})
+
+// Errors handlers
+
+app.use((req, res, next) => {
+  const err = new Error()
+  err.status = 404
+  err.message = 'Sorry, the page that you are looking for doesn\'t exist.'
+  console.error(`${err.message} Status Code: ${err.status}`)
+  next(err)
+})
+
+app.use((err, req, res, next) => {
+  if (err.status === 404) {
+    res.render('page-not-found', { err })
+  } else {
+    err.status = 500
+    err.message = 'There is something wrong with the code. Please try again later.'
+    console.log(`${err.message} Status Code: ${err.status}`)
+    res.render('error', { err })
+  }
+  return next()
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
